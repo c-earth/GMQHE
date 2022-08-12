d96503ffc1d302be724a4bfbcc3c1fe593a34ec2
Abhijatmedhi Chotrattanapituk <40755638+c-earth@users.noreply.github.com>
2022-08-12 16:49:49 -0400

    Add files via upload

--- gmqhe.py
+++ gmqhe.py
@@ -1,1 +1,26 @@
+#############################
+# main file for running GMQHE
+#############################
 
+# import modules
+from utils.data_prep import TB_model
+
+# work flow settings
+regenerate_tb_models = True
+rebuild_speciments = True
+
+# paths settings
+raw_data_folder = './data/raw'
+tb_models_folder = './data/TB_models'
+speciments_folder = './data/speciments'
+
+# load data
+TB_models = TB_model.from_folder(raw_data_folder)
+
+# generate plain structures
+
+# etch holes
+
+# attach leads
+
+# measure conductances
